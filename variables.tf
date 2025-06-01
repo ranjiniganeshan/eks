8816c8e3bc77d6212472d0674f9e122056c1e94f
ranjiniganeshan <ranjiniganeshan@gmail.com>
2025-06-01 15:18:22 +0530

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,12 @@
+# Defining variables
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
+}
